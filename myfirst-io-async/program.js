1a177ea6bd192adac6f84727baa2302431612f20
ackuser <mail@ackarim.com>
2015-09-18 15:33:10 +0100

    Adding solutions

--- myfirst-io-async/program.js
+++ myfirst-io-async/program.js
@@ -7,20 +7,3 @@ fs.readFile(process.argv[2], "utf-8", function (err, data) {
 });
 /* EXAMPLE OF EXECUTION */
 //node myfirst-io/program.js /opt/mean/learnyounode-tasks/myfirst-io/todo.md
-
-
-/*
-Here's the official solution in case you want to compare notes:
-
-────────────────────────────────────────────────────────────────────────────────
-    var fs = require('fs')
-    var file = process.argv[2]
-
-    fs.readFile(file, function (err, contents) {
-      // fs.readFile(file, 'utf8', callback) can also be used
-      var lines = contents.toString().split('\n').length - 1
-      console.log(lines)
-    })
-
-────────────────────────────────────────────────────────────────────────────────
-*/
